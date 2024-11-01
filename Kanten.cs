102c28c33632fd8502a8522b369e01983e9d54fd
Coolplay88 <126912070+Coolplay88@users.noreply.github.com>
2024-11-01 22:01:56 +0100

    Create Kanten.cs

---
+++ Kanten.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kanten : MonoBehaviour
+{
+    public int in_index;
+    public int out_index;
+    public float weight;
+    public bool activated;
+    public int innovation_number;
+
+
+}
+
