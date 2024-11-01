48eb2a9aa98050bf8d0056998285cc9bef0b7671
Coolplay88 <126912070+Coolplay88@users.noreply.github.com>
2024-11-01 22:02:17 +0100

    Create ActivationFunctions.cs

---
+++ ActivationFunctions.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationFunctions : MonoBehaviour
+{
+    public enum ActivationType
+    {
+        Sigmoid,
+        Tanh,
+        ReLU,
+        LeakyReLU,
+        ELU
+    }
+
+}
